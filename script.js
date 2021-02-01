829da226dc695322426b5171392f6975b38d290e
Matheus Martino <matheus.martino97@gmail.com>
2021-02-01 17:24:03 -0300

    Fixing code climate issues.

--- script.js
+++ script.js
@@ -20,7 +20,7 @@ function createProductItemElement({ sku, name, image }) {
   section.appendChild(createCustomElement('span', 'item__title', name));
   section.appendChild(createProductImageElement(image));
   section.appendChild(
-    createCustomElement('button', 'item__add', 'Adicionar ao carrinho!')
+    createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'),
   );
 
   return section;
@@ -28,6 +28,14 @@ function createProductItemElement({ sku, name, image }) {
 
 function getSkuFromProductItem(item) {
   return item.querySelector('span.item__sku').innerText;
+}
+
+function createCartItemElement({ sku, name, salePrice }) {
+  const li = document.createElement('li');
+  li.className = 'cart__item';
+  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
+  li.addEventListener('click', cartItemClickListener);
+  return li;
 }
 
 function cartItemClickListener() {
@@ -39,8 +47,8 @@ function cartItemClickListener() {
       const endpoint = `https://api.mercadolibre.com/items/${event.target.parentElement.firstChild.innerText}`;
 
       fetch(endpoint)
-        .then((response) => response.json())
-        .then((data) => {
+        .then(response => response.json())
+        .then(data => {
           const productInfo = {
             sku: data.id,
             name: data.title,
@@ -51,17 +59,9 @@ function cartItemClickListener() {
 
           cartSection.appendChild(cartItemElement);
         })
-        .catch((error) => window.alert(error));
+        .catch(error => window.alert(error));
     }
   });
-}
-
-function createCartItemElement({ sku, name, salePrice }) {
-  const li = document.createElement('li');
-  li.className = 'cart__item';
-  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-  li.addEventListener('click', cartItemClickListener);
-  return li;
 }
 
 function fetchAPI() {
@@ -69,8 +69,8 @@ function fetchAPI() {
   const itemsSection = document.querySelector('.items');
 
   fetch(endpoint)
-    .then((response) => response.json())
-    .then((data) => {
+    .then(response => response.json())
+    .then(data => {
       data.results.forEach((result) => {
         const obj = {
           sku: result.id,
