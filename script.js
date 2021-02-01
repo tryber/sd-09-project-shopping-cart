012738f7d9c2d67128e8fa62a11b1533fa3caf57
ederson rodrigo <edersonrodrigo31@gmail.com>
2021-02-01 17:15:53 -0300

    evaluator fix

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-window.onload = function onload() { };
 function createProductImageElement(imageSource) {
   const img = document.createElement('img');
   img.className = 'item__image';
@@ -6,12 +5,16 @@ function createProductImageElement(imageSource) {
   return img;
 }
 
+function cartItemClickListener(event){
+
+}
+
 function createCartItemElement({ sku, name, salePrice }) {
   const ol = document.querySelector('ol');
   const li = document.createElement('li');
   li.className = 'cart__item';
   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-  // li.addEventListener('click', cartItemClickListener);
+  li.addEventListener('click', cartItemClickListener);
 
   ol.appendChild(li);
   return li;
@@ -57,7 +60,7 @@ function createProductItemElement({ sku, name, image }) {
   return section;
 }
 
-function cartItemClickListener(event) {
+function showProducts() {
   const api = 'https://api.mercadolibre.com/sites/MLB/search?q=computador';
 
   fetch(api)
@@ -73,4 +76,6 @@ function getSkuFromProductItem(item) {
   return item.querySelector('span.item__sku').innerText;
 }
 
-cartItemClickListener(window.onload);
+window.onload = function onload() {
+  showProducts();
+};
