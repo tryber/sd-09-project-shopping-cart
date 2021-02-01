e6b0f222b71f66e901e4cb32c96c996b90f756d9
Pedro Henrique <pedro.876.ph@gmail.com>
2021-02-01 18:42:23 -0300

    requisito 2

--- script.js
+++ script.js
@@ -28,15 +28,47 @@ function getSkuFromProductItem(item) {
   return item.querySelector('span.item__sku').innerText;
 }
 
-function fetchMercadoLivre(search) {
+async function fecthIDs(id) {
+  const endPoint = `https://api.mercadolibre.com/items/${id}`;
+  try {
+    const response = await fetch(endPoint);
+    const responseJson = await response.json();
+    const obj = {
+      ...responseJson,
+      sku: responseJson.id,
+      name: responseJson.title,
+      salePrice: responseJson.price,
+    }
+    const cartItem = createCartItemElement(obj);
+    document.querySelector('.cart__items').appendChild(cartItem);
+  } catch (error) {
+    alert(error);
+  }
+}
+
+function buttonCartListener() {
+  const buttonAddCart = document.querySelectorAll('.item__add');
+  // console.log(buttonAddCart);
+  buttonAddCart.forEach((element) => {
+    element.addEventListener('click', (event) => {
+      const idSku = event.path[1].firstElementChild.innerText;
+      fecthIDs(idSku);
+    })
+  })
+}
+
+function fetchProducts(search) {
   const endPoint = `https://api.mercadolibre.com/sites/MLB/search?q=${search}`;
   fetch(endPoint)
     .then(response => response.json())
     .then(obj => obj.results.forEach(({ id, title, thumbnail }) => {
       const product = createProductItemElement({ sku: id, name: title, image: thumbnail });
       document.querySelector('.items').appendChild(product);
-    }));
+    }))
+    .then(buttonCartListener)
 }
+
+
 
 function cartItemClickListener(event) {
   // coloque seu código aqui
@@ -51,5 +83,5 @@ function createCartItemElement({ sku, name, salePrice }) {
 }
 
 window.onload = function onload() {
-  fetchMercadoLivre('computador');
+  fetchProducts('computador');
 };
