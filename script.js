74a405a1c41ce0144a9f664c682df2ed80c11e0a
Matheus Martino <matheus.martino97@gmail.com>
2021-02-01 19:44:29 -0300

    Fixing code climate issues.

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function createProductImageElement(imageSource) {
-  const img = document.createElement("img");
-  img.className = "item__image";
+  const img = document.createElement('img');
+  img.className = 'item__image';
   img.src = imageSource;
   return img;
 }
@@ -13,25 +13,25 @@ function createCustomElement(element, className, innerText) {
 }
 
 function createProductItemElement({ sku, name, image }) {
-  const section = document.createElement("section");
-  section.className = "item";
+  const section = document.createElement('section');
+  section.className = 'item';
 
-  section.appendChild(createCustomElement("span", "item__sku", sku));
-  section.appendChild(createCustomElement("span", "item__title", name));
+  section.appendChild(createCustomElement('span', 'item__sku', sku));
+  section.appendChild(createCustomElement('span', 'item__title', name));
   section.appendChild(createProductImageElement(image));
   section.appendChild(
-    createCustomElement("button", "item__add", "Adicionar ao carrinho!"),
+    createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'),
   );
 
   return section;
 }
 
 function getSkuFromProductItem(item) {
-  return item.querySelector("span.item__sku").innerText;
+  return item.querySelector('span.item__sku').innerText;
 }
 
 function updatePriceMinus(event) {
-  const totalPriceSpan = document.querySelector("#total-price");
+  const totalPriceSpan = document.querySelector('#total-price');
   const priceToBeSubtracted = parseFloat(event.target.id);
   let parseTotalPrice = parseFloat(totalPriceSpan.innerText);
 
@@ -40,24 +40,24 @@ function updatePriceMinus(event) {
 }
 
 function cartItemClickListener() {
-  const cartItemsOrderedList = document.querySelector(".cart__items");
+  const cartItemsOrderedList = document.querySelector('.cart__items');
 
-  cartItemsOrderedList.addEventListener("click", (event) => {
+  cartItemsOrderedList.addEventListener('click', (event) => {
     cartItemsOrderedList.removeChild(event.target);
     updatePriceMinus(event);
   });
 }
 
 function createCartItemElement({ sku, name, salePrice }) {
-  const li = document.createElement("li");
-  li.className = "cart__item";
+  const li = document.createElement('li');
+  li.className = 'cart__item';
   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-  li.addEventListener("click", cartItemClickListener);
+  li.addEventListener('click', cartItemClickListener);
   return li;
 }
 
 function updatePriceSum(price) {
-  const totalPriceSpan = document.querySelector("#total-price");
+  const totalPriceSpan = document.querySelector('#total-price');
   let parseTotalPrice = parseFloat(totalPriceSpan.innerText);
 
   parseTotalPrice += price;
@@ -65,8 +65,8 @@ function updatePriceSum(price) {
 }
 
 function addItems(event) {
-  if (event.target.className === "item__add") {
-    const cartSection = document.querySelector(".cart__items");
+  if (event.target.className === 'item__add') {
+    const cartSection = document.querySelector('.cart__items');
     const endpoint = `https://api.mercadolibre.com/items/${event.target.parentElement.firstChild.innerText}`;
 
     fetch(endpoint)
@@ -89,14 +89,14 @@ function addItems(event) {
 }
 
 function addItemsClickListener() {
-  const itemAddButtonNodeList = document.querySelector(".items");
+  const itemAddButtonNodeList = document.querySelector('.items');
 
-  itemAddButtonNodeList.addEventListener("click", addItems);
+  itemAddButtonNodeList.addEventListener('click', addItems);
 }
 
 function fetchAPI() {
-  const endpoint = "https://api.mercadolibre.com/sites/MLB/search?q=computador";
-  const itemsSection = document.querySelector(".items");
+  const endpoint = 'https://api.mercadolibre.com/sites/MLB/search?q=computador';
+  const itemsSection = document.querySelector('.items');
 
   fetch(endpoint)
     .then((response) => response.json())
@@ -109,7 +109,7 @@ function fetchAPI() {
         };
 
         itemsSection.appendChild(createProductItemElement(obj));
-        document.querySelector(".load").style.display = "none";
+        document.querySelector('.load').style.display = 'none';
       });
     })
     .catch((error) => {
