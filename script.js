d171868aec7ce4168659bedbd719d3314931e37e
Rafael Geronimo <rafaelgeronimo@outlook.com>
2021-02-01 18:59:48 -0300

    Feature: Add product to cart

--- script.js
+++ script.js
@@ -5,10 +5,30 @@ function createProductImageElement(imageSource) {
   return img;
 }
 
-function createCustomElement(element, className, innerText) {
+const cartItemClickListener = async(itemID) => {
+  try {
+    const productDetails = `https://api.mercadolibre.com/items/${itemID}`;
+    await fetch(productDetails)
+      .then((response) => response.json())
+      .then((object) => {
+        const newProduct = createCartItemElement(object);
+        document.querySelector('.cart__items').appendChild(newProduct);
+      });
+  } catch (error) {
+    console.log(`Ocorreu um erro: ${error}`);
+  }
+}
+
+function createCustomElement(element, className, innerText, sku) {
   const e = document.createElement(element);
   e.className = className;
   e.innerText = innerText;
+  if (element === 'button') {
+    e.id = sku;
+    e.addEventListener('click', element => {
+      cartItemClickListener(e.id);
+    });
+}
   return e;
 }
 
@@ -18,7 +38,7 @@ function createProductItemElement({ id: sku, title: name, thumbnail: image }) {
   section.appendChild(createCustomElement('span', 'item__sku', sku));
   section.appendChild(createCustomElement('span', 'item__title', name));
   section.appendChild(createProductImageElement(image));
-  section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
+  section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!', sku));
   return section;
 }
 
@@ -26,11 +46,7 @@ function getSkuFromProductItem(item) {
   return item.querySelector('span.item__sku').innerText;
 }
 
-function cartItemClickListener(event) {
-  // coloque seu código aqui
-}
-
-function createCartItemElement({ sku, name, salePrice }) {
+function createCartItemElement({ id: sku, title: name, price: salePrice }) {
   const li = document.createElement('li');
   li.className = 'cart__item';
   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
@@ -48,10 +64,14 @@ const fetchItems = async (product) => {
         document.querySelector('.items').appendChild(itemElement);
       }));
   } catch (error) {
-    console.log(`Deu um erro aqui: ${error}`);
+    console.log(`Houve um erro: ${error}`);
   }
 };
 
+const setupEvents = () => {
+  fetchItems('computador');   
+}
+
 window.onload = function onload() {
-  fetchItems('computador');
+  setupEvents();
 };
