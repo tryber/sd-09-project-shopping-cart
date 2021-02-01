1869d70ab65f1e2c8ab9ba1e9b0c42c09d8e5ad4
Nathália <nathaliaveneziano@icloud.com>
2021-02-01 19:52:02 -0300

    Requisito - Remova o item do carrinho de compras ao clicar nele

--- script.js
+++ script.js
@@ -29,8 +29,7 @@ function getSkuFromProductItem(item) {
 }
 
 function cartItemClickListener(event) {
-  // coloque seu código aqui
-  // console.log(event)
+  event.target.remove();
 }
 
 function createCartItemElement({ sku, name, salePrice }) {
