06f9784dc1dbfa83686fe157e3b7a9ab031feecf
caabeatriz <contatocarolinebs@gmail.com>
2021-02-01 11:10:01 -0300

    added test

--- script.js
+++ script.js
@@ -3,8 +3,9 @@ window.onload = function onload() { };
 function createProductImageElement(imageSource) {
   const img = document.createElement('img');
   img.className = 'item__image';
-  img.src = imageSource;
-  return img;
+  img.src = imageSource
+  console.log("teste")
+  return img
 }
 
 function createCustomElement(element, className, innerText) {
