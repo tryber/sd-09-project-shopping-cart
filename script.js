d7aad9f015a60ac6e33a6ca19b4c39fdf3c9d772
tiago-yoneda <xyonedax@gmail.com>
2021-02-01 16:35:21 -0300

    cria a lista + adiciona no cart

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
-window.onload = function onload() { };
+window.onload = function onload() {
+  document.querySelector('.empty-cart').addEventListener('click', () => {
+    document.querySelector('.cart__items').innerHTML = '';
+  });
+};
 
 function createProductImageElement(imageSource) {
   const img = document.createElement('img');
@@ -30,14 +34,60 @@ function getSkuFromProductItem(item) {
   return item.querySelector('span.item__sku').innerText;
 }
 
-function cartItemClickListener(event) {
-  // coloque seu código aqui
-}
+// function cartItemClickListener(event) {
+//   // coloque seu código aqui
+// }
 
 function createCartItemElement({ sku, name, salePrice }) {
   const li = document.createElement('li');
   li.className = 'cart__item';
   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-  li.addEventListener('click', cartItemClickListener);
+  // li.addEventListener('click', cartItemClickListener);
   return li;
 }
+
+const sendToCart = async (event) => {
+  const mySku = getSkuFromProductItem(event.target.parentNode);
+  const endpoint = `https://api.mercadolibre.com/items/${mySku}`;
+
+  const myResponse = await fetch(endpoint)
+  .then(response => response.json());
+
+  const myItem = {
+    sku: mySku,
+    name: myResponse.title,
+    salePrice: myResponse.price,
+  };
+
+  const myListItem = createCartItemElement(myItem);
+  document.querySelector('.cart__items').appendChild(myListItem);
+};
+
+const addSendToCart = () => {
+  const myButtons = document.querySelectorAll('.item__add');
+  myButtons.forEach((button) => {
+    button.addEventListener('click', sendToCart);
+  });
+};
+
+const fetchSearch = async (query) => {
+  const endpoint = `https://api.mercadolibre.com/sites/MLB/search?q=$${query}`;
+
+  const resultArray = await fetch(endpoint)
+  .then(response => response.json())
+  .then(object => object.results);
+
+  for (let index = 0; index < 20; index += 1) {
+    const myObject = {
+      name: resultArray[index].title,
+      sku: resultArray[index].id,
+      image: resultArray[index].thumbnail,
+    };
+    const myItem = createProductItemElement(myObject);
+    document.querySelector('.items').appendChild(myItem);
+  }
+
+  addSendToCart();
+};
+
+fetchSearch('Computador');
