eb772f4716d29f1474508af1cd9022394cb0eb9a
ederson rodrigo <edersonrodrigo31@gmail.com>
2021-02-01 16:51:52 -0300

    adicionando produto no carrinho

--- script.js
+++ script.js
@@ -1,55 +1,75 @@
-window.onload = function onload() {};
+window.onload = function onload() {
+  function createProductImageElement(imageSource) {
+    const img = document.createElement('img');
+    img.className = 'item__image';
+    img.src = imageSource;
+    return img;
+  }
 
-function createProductImageElement(imageSource) {
-  const img = document.createElement('img');
-  img.className = 'item__image';
-  img.src = imageSource;
-  return img;
-}
+  function createCustomElement(element, className, innerText) {
+    const e = document.createElement(element);
+    e.className = className;
+    e.innerText = innerText;
 
-function createCustomElement(element, className, innerText) {
-  const e = document.createElement(element);
-  e.className = className;
-  e.innerText = innerText;
-  return e;
-}
+    if (element == 'button') {
+      e.addEventListener('click', function () {
+        let elementoPai = e.parentNode;
+        let id = elementoPai.childNodes[0].innerText;
+        const link = `https://api.mercadolibre.com/items/${id}`;
 
-function createProductItemElement({ sku, name, image }) {
-  const items = document.querySelector('.items');
-  const section = document.createElement('section');
-  section.className = 'item';
+        fetch(link)
+          .then(response => response.json()
+            .then((e) => {
+              createCartItemElement({ sku: e.id, name: e.title, salePrice: e.price });
+            }));
+      });
+    }
+    return e;
+  }
 
-  section.appendChild(createCustomElement('span', 'item__sku', sku));
-  section.appendChild(createCustomElement('span', 'item__title', name));
-  section.appendChild(createProductImageElement(image));
-  section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
+  function createProductItemElement({ sku, name, image }) {
+    const items = document.querySelector('.items');
+    const section = document.createElement('section');
+    section.className = 'item';
 
-  items.appendChild(section);
-  return section;
-}
+    section.appendChild(createCustomElement('span', 'item__sku', sku));
+    section.appendChild(createCustomElement('span', 'item__title', name));
+    section.appendChild(createProductImageElement(image));
+    section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
+
+    items.appendChild(section);
+
+    return section;
+  }
+
+  function cartItemClickListener(event) {
+    const api = 'https://api.mercadolibre.com/sites/MLB/search?q=computador';
+
+    fetch(api)
+      .then(response => response.json()
+        .then((elements) => {
+          elements.results.forEach((e) => {
+            createProductItemElement({ sku: e.id, name: e.title, image: e.thumbnail });
+          });
+        }));
+  }
+
+  function createCartItemElement({ sku, name, salePrice }) {
+    const ol = document.querySelector('ol');
+    const li = document.createElement('li');
+    li.className = 'cart__item';
+    li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
+    li.addEventListener('click', cartItemClickListener);
+
+    ol.appendChild(li);
+    return li;
+  }
+  cartItemClickListener();
+};
+
 
 function getSkuFromProductItem(item) {
   return item.querySelector('span.item__sku').innerText;
 }
 
-function cartItemClickListener(event) {
-  const api = 'https://api.mercadolibre.com/sites/MLB/search?q=computador';
 
-  fetch(api)
-    .then(response => response.json()
-      .then((elements) => {
-        elements.results.forEach((e) => {
-          createProductItemElement({ sku: e.id, name: e.title, image: e.thumbnail });
-        });
-      }));
-}
-
-function createCartItemElement({ sku, name, salePrice }) {
-  const li = document.createElement('li');
-  li.className = 'cart__item';
-  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-  li.addEventListener('click', cartItemClickListener);
-  return li;
-}
-
-cartItemClickListener(window.onload);
