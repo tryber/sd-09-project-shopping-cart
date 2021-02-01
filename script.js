f356ef3264db9d84274924b18e0acf22d4477f95
Pedro Henrique <pedroenrique.400@gmail.com>
2021-02-01 17:07:57 -0300

    Faz o primeiro requisito

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ function createCustomElement(element, className, innerText) {
 function createProductItemElement({ sku, name, image }) {
   const section = document.createElement('section');
   section.className = 'item';
-
+ 
   section.appendChild(createCustomElement('span', 'item__sku', sku));
   section.appendChild(createCustomElement('span', 'item__title', name));
   section.appendChild(createProductImageElement(image));
@@ -29,10 +29,42 @@ function createProductItemElement({ sku, name, image }) {
 function getSkuFromProductItem(item) {
   return item.querySelector('span.item__sku').innerText;
 }
+  const handleWithSearchResults = (object) => {
+    object.results.forEach((result) => {
+      const infosComput = {}
+      infosComput['sku'] = result.id;
+      infosComput['name'] = result.title;
+      infosComput['image'] = result.thumbnail;
+      const section = document.querySelector('.items');
+      section.appendChild(createProductItemElement(infosComput));
+    });
+
+  }
+
+  const fetchComputers =  (endpoint) => {
+    console.log(endpoint);
+    fetch(endpoint)
+      .then((response) => response.json())
+      .then((object) => {
+        if(object.results.length === 0) {
+          error = 'Produto não existe';
+          throw new Error(error);
+        }
+        handleWithSearchResults(object);
+      })
+    .catch((error) => {
+      window.alert(error);
+    });
+  }
+function createStoreItens() {
+  const endpoint = 'https://api.mercadolibre.com/sites/MLB/search?q=computador';
+  fetchComputers(endpoint);
+}
 
 function cartItemClickListener(event) {
   // coloque seu código aqui
-  // INICIANDO
+  
+  
 }
 
 function createCartItemElement({ sku, name, salePrice }) {
@@ -42,3 +74,7 @@ function createCartItemElement({ sku, name, salePrice }) {
   li.addEventListener('click', cartItemClickListener);
   return li;
 }
+
+// Chamada de Funções 
+createStoreItens();
+
