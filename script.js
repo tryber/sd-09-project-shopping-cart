8f031e6810397c33171ee711cf1ca26fed01fd34
Lenilsom Ferreira da Silva <silvalenilsomsud@gmail.com>
2021-02-01 15:25:07 -0300

    commit inicial

--- script.js
+++ script.js
@@ -32,6 +32,7 @@ function getSkuFromProductItem(item) {
 
 function cartItemClickListener(event) {
   // coloque seu código aqui
+  console.log('novoprojeto');
 }
 
 function createCartItemElement({ sku, name, salePrice }) {
