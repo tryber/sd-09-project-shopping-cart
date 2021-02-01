ead1889d5f4ca21cc43b6519cac700ee38727ef3
Felipe Shcolnik <shcolnik@gmail.com.br>
2021-02-01 19:08:43 -0300

    exercicio 1 codeclimate2

--- script.js
+++ script.js
@@ -1,31 +1,3 @@
-
-
-const fetchAllProducts = () => {
-  const query = 'computador';
-  const endpoint = `https://api.mercadolibre.com/sites/MLB/search?q=${query}`;
-  fetch(endpoint)
-    .then(response => response.json())
-    .then(dataAll => {
-      if (dataAll.error) {
-        throw new Error(dataAll.error);
-      }
-      const allProductsInfo = dataAll.results;
-      allProductsInfo.forEach(object => {
-        const { id, title, thumbnail } = object;
-        const item = createProductItemElement({
-          sku: id,
-          name: title,
-          image: thumbnail });
-        document.querySelector('.items').appendChild(item);
-      });
-    })
-    .catch((error) => {
-      window.alert(error);
-    });
-}
-
-
-
 function createProductImageElement(imageSource) {
   const img = document.createElement('img');
   img.className = 'item__image';
@@ -65,6 +37,30 @@ function createCartItemElement({ sku, name, salePrice }) {
   li.addEventListener('click', cartItemClickListener);
   return li;
 }
+const fetchAllProducts = () => {
+  const query = 'computador';
+  const endpoint = `https://api.mercadolibre.com/sites/MLB/search?q=${query}`;
+  fetch(endpoint)
+    .then(response => response.json())
+    .then((dataAll) => {
+      if (dataAll.error) {
+        throw new Error(dataAll.error);
+      }
+      const allProductsInfo = dataAll.results;
+      allProductsInfo.forEach((object) => {
+        const { id, title, thumbnail } = object;
+        const item = createProductItemElement({
+          sku: id,
+          name: title,
+          image: thumbnail });
+        document.querySelector('.items').appendChild(item);
+      });
+    })
+    .catch((error) => {
+      window.alert(error);
+    });
+};
+
 window.onload = function onload() {
   fetchAllProducts();
 };
