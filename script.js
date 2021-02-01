e84f094f9dffb5a0fd5d1431e3308be2558c3731
Cristiano Roberto <cristianoroberto@outlook.com>
2021-02-01 15:22:43 -0300

    Iniciando o projeto Shopping Cart. VAMOS COM TUDO!

--- script.js
+++ script.js
@@ -31,7 +31,7 @@ function getSkuFromProductItem(item) {
 }
 
 function cartItemClickListener(event) {
-  // coloque seu código aqui
+  // Iniciando Aqui!
 }
 
 function createCartItemElement({ sku, name, salePrice }) {
