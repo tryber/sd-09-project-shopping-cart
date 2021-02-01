8461aa770b5b787cadb9ecd434131118027c221a
Thiago da Silva Gomes <thiagosilvagomes@gmail.com>
2021-02-01 14:22:15 -0300

    iniciando o projeto. VAMOS COM TUDO :rocket:

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ function createCustomElement(element, className, innerText) {
   const e = document.createElement(element);
   e.className = className;
   e.innerText = innerText;
-  return e;
+  return e; 
 }
 
 function createProductItemElement({ sku, name, image }) {
