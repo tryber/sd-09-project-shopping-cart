c0452de553241307e431618603db43bc11279ada
LucioOSilva <lucioosilva00@gmail.com>
2021-02-01 16:58:47 -0300

    added function add item to cart- CC2

--- script.js
+++ script.js
@@ -12,6 +12,22 @@ function createCustomElement(element, className, innerText) {
   return e;
 }
 
+function getSkuFromProductItem(item) {
+  return item.querySelector('span.item__sku').innerText;
+}
+
+function cartItemClickListener() {
+  setTimeout(() => {
+    const buttonAddToList = document.querySelectorAll('.item__add');
+    buttonAddToList.forEach((button) => {
+      button.addEventListener('click', function () {
+        const itemID = getSkuFromProductItem(button.parentNode);
+        searchItemById(itemID);
+      });
+    });
+  }, 500);
+}
+
 function createProductItemElement({ sku, name, image }) {
   const section = document.createElement('section');
   section.className = 'item';
@@ -30,10 +46,6 @@ function createCartItemElement({ sku, name, salePrice }) {
   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
   li.addEventListener('click', cartItemClickListener);
   return li;
-}
-
-function getSkuFromProductItem(item) {
-  return item.querySelector('span.item__sku').innerText;
 }
 
 function addToList(data, objectComplement, sectionToAdd, callback) {
@@ -62,18 +74,6 @@ function searchItemById(id) {
       const obj = { salePrice: data.price };
       addToList(data, obj, document.querySelector('.cart__items'), createCartItemElement);
     });
-}
-
-function cartItemClickListener() {
-  setTimeout(() => {
-    const buttonAddToList = document.querySelectorAll('.item__add');
-    buttonAddToList.forEach((button) => {
-      button.addEventListener('click', function () {
-        const itemID = getSkuFromProductItem(button.parentNode);
-        searchItemById(itemID);
-      });
-    });
-  }, 500);
 }
 
 window.onload = () => {
