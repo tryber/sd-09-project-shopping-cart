f3fe59b71ac11f0a8f7954c0f850bd165950a977
Christopher <chris.tryber@gmail.com>
2021-02-02 10:58:03 -0300

    Criei a listagem de produtos

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-window.onload = function onload() { };
+window.onload = function onload() {
+  createListing('computador')
+ };
 
 function createProductImageElement(imageSource) {
   const img = document.createElement('img');
@@ -12,6 +14,38 @@ function createCustomElement(element, className, innerText) {
   e.className = className;
   e.innerText = innerText;
   return e;
+}
+
+  // Criar uma listagem de produtos
+  // A listagem deve ser consultada na API do mercado livre
+  // Deve ser usado o seguinte endpoint: https://api.mercadolibre.com/sites/MLB/search?q=$QUERY
+  // A $QUERY deve ser o que buscar (nesse caso deve ser 'computador')
+  // A lista deve ser o array results
+  // Na função createProductItemElement() criar os componetes HTML referentes ao produto
+  // Adicione o elemento retornado da função como filho do elemento <section class="items">
+  // sku = id && thumbnail = image
+
+  // Código Português
+  // 1. Encontrar onde estão os produtos -- OK
+  // 2. Encontrar o array results -- OK
+  // 3. Utilizar a função createProductItemElement() para criar os componetes HTML -- OK
+  // 4. Colocar o retorno da função createProductItemElement() como filho da section.items -- OK
+
+  // Código JS
+
+function createListing(search) {
+  const sectionItems = document.querySelector('.items');
+  const promiseAPI = fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${search}`)
+    .then((response) => response.json());
+  const arrayResults = promiseAPI.then(response => response.results);
+  arrayResults.then(response => response.forEach((value) => {
+   resultOgj = {
+      sku: value.id,
+      name: search,
+      image: value.thumbnail,
+    };
+    sectionItems.appendChild(createProductItemElement(resultOgj));
+  }));
 }
 
 function createProductItemElement({ sku, name, image }) {
