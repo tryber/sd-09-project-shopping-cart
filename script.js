754b25ab77ee966e5d04721ccd7383e40cd880c6
jiarguello <jhonatan.arguello@gmail.com>
2021-02-02 16:19:32 -0300

    Atualizaçao de issues para codeclimate.

--- script.js
+++ script.js
@@ -3,14 +3,33 @@ function createProductImageElement(imageSource) {
   img.className = 'item__image';
   img.src = imageSource;
   return img;
-}
+};
+
+function createCustomElement(element, className, innerText) {
+  const e = document.createElement(element);
+  e.className = className;
+  e.innerText = innerText;
+  return e;
+};
+
+function createProductItemElement({ sku, name, image }) {
+  const section = document.createElement('section');
+  section.className = 'item';
+
+  section.appendChild(createCustomElement('span', 'item__sku', sku));
+  section.appendChild(createCustomElement('span', 'item__title', name));
+  section.appendChild(createProductImageElement(image));
+  section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
+  
+  return section;
+};
 
 const returnOfAPI = async (computador) => {
   const endPoint = `https://api.mercadolibre.com/sites/MLB/search?q=${computador}`;
 
   const retrieveOfAPI = await fetch(endPoint)
     .then(response => response.json())
-    .then((object) => object.results)
+    .then(object => object.results)
     .catch(error => console.log(error));
 
   retrieveOfAPI.forEach((element) => {
@@ -24,28 +43,9 @@ const returnOfAPI = async (computador) => {
   });
 };
 
-function createCustomElement(element, className, innerText) {
-  const e = document.createElement(element);
-  e.className = className;
-  e.innerText = innerText;
-  return e;
-}
-
-function createProductItemElement({ sku, name, image }) {
-  const section = document.createElement('section');
-  section.className = 'item';
-
-  section.appendChild(createCustomElement('span', 'item__sku', sku));
-  section.appendChild(createCustomElement('span', 'item__title', name));
-  section.appendChild(createProductImageElement(image));
-  section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
-
-  return section;
-}
-
 function getSkuFromProductItem(item) {
   return item.querySelector('span.item__sku').innerText;
-}
+};
 
 function cartItemClickListener(event) {
   // coloque seu código aqui
