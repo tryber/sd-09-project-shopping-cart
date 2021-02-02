0cf2f055ddcdf2f450bddefeac797ad3e425bb64
MarcioFujita <hanker78@gmail.com>
2021-02-02 19:21:17 -0300

    Preparo requisito 4 v8

--- script.js
+++ script.js
@@ -72,11 +72,11 @@ const fetchSelectedItem = async (event) => {
     const result = createCartItemElement({ sku, name, salePrice, id });
     const cartItens = document.querySelector('.cart__items');
     cartItens.appendChild(result);
-    objLocalStorage.push({ 
-    sku: sku, 
-    name: name, 
-    salePrice: salePrice, 
-    id: id });
+    objLocalStorage.push({
+      sku: sku,
+      name: name,
+      salePrice: salePrice,
+      id: id });
     localStorage.setItem('MLCart', JSON.stringify(objLocalStorage));
   } catch (error) {
     window.alert(error);
