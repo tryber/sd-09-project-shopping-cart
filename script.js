d29295e30eb2679c65ac5b7a120938d1c9d947eb
Enio Nicael <enionicael@gmail.com>
2021-02-02 16:06:42 -0300

    Requisito 2 pega o id do item quando o botao for clicado.

--- script.js
+++ script.js
@@ -28,6 +28,21 @@ function getSkuFromProductItem(item) {
   return item.querySelector('span.item__sku').innerText;
 }
 
+function handleClickAddToCart(event) {
+  const sectionItem = event.target.parentNode;
+  const sku = getSkuFromProductItem(sectionItem);
+  // cartItemClickListener(sku);
+
+  console.log(sku);
+}
+
+function addEventInAddToCartButton() {
+  const buttons = document.querySelectorAll('.item__add');
+  buttons.forEach((button) => {
+    button.addEventListener('click', handleClickAddToCart);
+  });
+}
+
 const mapProducts = (product) => {
   const { id, title, thumbnail, price } = product;
   return {
@@ -38,32 +53,34 @@ const mapProducts = (product) => {
   };
 };
 
-function getfetchProductsResult({ results }) {
+const getfetchProductsResult = ({ results }) => {
   const filterData = results.map(mapProducts);
   filterData.forEach((product) => {
     const sectionItems = document.querySelector('.items');
     const item = createProductItemElement(product);
     sectionItems.appendChild(item);
   });
-}
+  addEventInAddToCartButton();
+};
 
-function fetchProducts() {
-  const endpoint = 'https://api.mercadolibre.com/sites/MLB/search?q=$computador';
-
-  fetch(endpoint)
-    .then(response => response.json())
-    .then((object) => {
-      if (object.results.length === 0) {
-        throw new Error('Produto nao encontrado!');
-      }
-      getfetchProductsResult(object);
-    })
-    .catch(error => console.log(error));
+async function fetchProducts() {
+  const endpoint = 'https://api.mercadolibre.com/sites/MLB/search?q=computador';
+  try {
+    const response = await fetch(endpoint);
+    const searchResult = await response.json();
+    if (searchResult.results.length === 0) {
+      throw new Error('Produto nao encontrado!');
+    }
+    getfetchProductsResult(searchResult);
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 function cartItemClickListener(event) {
-  // coloque seu código aqui
-
+  // recuperar o botao referente ao click
+  alert(event.target);
+  const endpoint = 'https://api.mercadolibre.com/items/$ItemID';
 }
 
 function createCartItemElement({ sku, name, salePrice }) {
