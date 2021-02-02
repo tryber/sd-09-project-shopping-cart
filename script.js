346cf54cf14361049e83e4daf04d3c446bc71c66
Qu4k3r <neves0007@gmail.com>
2021-02-02 16:37:44 -0300

    Adicionado elemento de soma dos itens do carrinho

--- script.js
+++ script.js
@@ -12,6 +12,11 @@ function createCustomElement(element, className, innerText) {
   return e;
 }
 
+function totalPrice(price) {
+  const value = Number(document.querySelector('.total-price').innerText);
+  document.querySelector('.total-price').innerText = (value + price).toFixed(2);
+}
+
 function createProductItemElement({ sku, name, image }) {
   const section = document.createElement('section');
   section.className = 'item';
@@ -27,6 +32,10 @@ function createProductItemElement({ sku, name, image }) {
 function cartItemClickListener(event) {
   // coloque seu código aqui
   event.target.remove();
+  const totalPrice = document.querySelector('.total-price');
+  const priceTarget = Number(event.target.innerText.split('$')[1]);
+  localStorage.totalPrice -= priceTarget;
+  totalPrice.innerText = localStorage.totalPrice;
   localStorage.myCart = document.querySelector('.cart__items').innerHTML;
 }
 
@@ -60,8 +69,14 @@ function buttonsListener(button) {
   button.addEventListener('click', async () => {
     fetchListItem(getSkuFromProductItem(button.parentNode))
     .then(obj => obj.json())
-    .then(({ id: sku, title: name, price: salePrice }) => addItemToCart({ sku, name, salePrice }))
-    .then(result => localStorage.setItem('myCart', result.innerHTML))
+    .then(({ id: sku, title: name, price: salePrice }) => {
+      totalPrice(salePrice);
+      return addItemToCart({ sku, name, salePrice });
+    })
+    .then(result => {
+      localStorage.setItem('myCart', result.innerHTML);
+      localStorage.setItem('totalPrice', document.querySelector('.total-price').innerHTML);
+    })
     .catch(() => alert('Outro erro! Dá uma olhado nos botões "Adicionar ao carrinho"'));
   });
 }
@@ -88,14 +103,19 @@ function createProductsList(item) {
 
 function storageItems() {
   const myCart = document.querySelector('.cart__items');
+  const totalPrice = document.querySelector('.total-price');
   if (localStorage.myCart) {
     myCart.innerHTML = localStorage.myCart;
     myCart.childNodes.forEach(item => item.addEventListener('click', cartItemClickListener));
+  }
+  if (localStorage.totalPrice) {
+    totalPrice.innerText = localStorage.totalPrice;
   }
 }
 
 window.onload = function onload() {
   createProductsList('computador');
+  document.querySelector('.cart').appendChild(createCustomElement('span', 'total-price', 0));
   storageItems();
   buttonTest();
 };
