5e3d0949f9a614e91d3920fb16f394b2a46f373d
ederson rodrigo <edersonrodrigo31@gmail.com>
2021-02-02 16:57:59 -0300

    fix code climate

--- script.js
+++ script.js
@@ -91,14 +91,14 @@ function createProductItemElement({ sku, name, image }) {
 function getProducts() {
   const api = 'https://api.mercadolibre.com/sites/MLB/search?q=computador';
   const loading = document.querySelector('h1');
-    fetch(api)
+  fetch(api)
     .then(response => response.json()
       .then((elements) => {
         elements.results.forEach((e) => {
           createProductItemElement({ sku: e.id, name: e.title, image: e.thumbnail });
         });
       }));
-      loading.style.display = 'none'
+  loading.style.display = 'none';
 }
 
 function getCartProducts() {
