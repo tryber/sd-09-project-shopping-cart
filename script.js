e947168ddd34c78110399a12c1729f5e9d81cc1e
jiarguello <jhonatan.arguello@gmail.com>
2021-02-02 16:22:02 -0300

    Atualizaçao de issues requisito 1.

--- script.js
+++ script.js
@@ -3,14 +3,14 @@ function createProductImageElement(imageSource) {
   img.className = 'item__image';
   img.src = imageSource;
   return img;
-};
+}
 
 function createCustomElement(element, className, innerText) {
   const e = document.createElement(element);
   e.className = className;
   e.innerText = innerText;
   return e;
-};
+}
 
 function createProductItemElement({ sku, name, image }) {
   const section = document.createElement('section');
@@ -20,9 +20,9 @@ function createProductItemElement({ sku, name, image }) {
   section.appendChild(createCustomElement('span', 'item__title', name));
   section.appendChild(createProductImageElement(image));
   section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
-  
+
   return section;
-};
+}
 
 const returnOfAPI = async (computador) => {
   const endPoint = `https://api.mercadolibre.com/sites/MLB/search?q=${computador}`;
@@ -45,7 +45,7 @@ const returnOfAPI = async (computador) => {
 
 function getSkuFromProductItem(item) {
   return item.querySelector('span.item__sku').innerText;
-};
+}
 
 function cartItemClickListener(event) {
   // coloque seu código aqui
