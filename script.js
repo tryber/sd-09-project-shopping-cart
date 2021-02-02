34b4d71a42a7c6354b600d6306bea840d95f3703
Thiago Granville <thiagogranville@gmail.com>
2021-02-01 21:25:41 -0300

    Correções para o code climate

--- script.js
+++ script.js
@@ -28,14 +28,6 @@ function getSkuFromProductItem(item) {
   return item.querySelector('span.item__sku').innerText;
 }
 
-function createCartItemElement({ sku, name, salePrice }) {
-  const li = document.createElement('li');
-  li.className = 'cart__item';
-  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-  li.addEventListener('click', cartItemClickListener);
-  return li;
-}
-
 async function sumPrices() {
   try {
     let totalPrice = 0;
@@ -69,6 +61,14 @@ function cartItemClickListener(event) {
   const currentCart = cart.innerHTML;
   localStorage.setItem('cart', JSON.stringify(currentCart));
   createTotalPriceElement();
+}
+
+function createCartItemElement({ sku, name, salePrice }) {
+  const li = document.createElement('li');
+  li.className = 'cart__item';
+  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
+  li.addEventListener('click', cartItemClickListener);
+  return li;
 }
 
 function addProductToTarget(parentClass, productObject, callback) {
