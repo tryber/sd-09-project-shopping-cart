6638c312883d2c08276376a80e292c8146f4c335
Rafael Geronimo <rafaelgeronimo@outlook.com>
2021-02-02 02:55:59 -0300

    Feature: Remove Item on Click

--- script.js
+++ script.js
@@ -5,10 +5,15 @@ function createProductImageElement(imageSource) {
   return img;
 }
 
+function removeItem() {
+  this.parentNode.removeChild(this);
+}
+
 function createCartItemElement({ id: sku, title: name, price: salePrice }) {
   const li = document.createElement('li');
   li.className = 'cart__item';
   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
+  li.addEventListener('click', removeItem);
   return li;
 }
 
