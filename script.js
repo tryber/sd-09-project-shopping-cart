a15ab62b51af85d03265e56e2f6628ea90b385ec
RodrigoNeoEx <draja.acc@gmail.com>
2021-02-02 19:06:48 -0300

    climate

--- script.js
+++ script.js
@@ -22,20 +22,12 @@ function createProductItemElement({ sku, name, image }) {
   return section;
 }
 
-function createCartItemElement({ sku, name, salePrice }) {
-  const li = document.createElement('li');
-  li.className = 'cart__item';
-  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-  li.addEventListener('click', cartItemClickListener);
-  return li;
-}
-
 function getSkuFromProductItem(item) {
   return item.querySelector('span.item__sku').innerText;
 }
 
 function addItem() {
-  const buttonAdd = document.querySelectorAll('button.item__add')
+  const buttonAdd = document.querySelectorAll('button.item__add');
   buttonAdd.forEach((button) => {
     button.addEventListener('click', sendItemToCart);
   });
@@ -44,11 +36,11 @@ function addItem() {
 async function fetchItemById(itemId) {
   const endPoint = `https://api.mercadolibre.com/items/${itemId}`;
   const response = await fetch(endPoint);
-  return await response.json();
+  return response.json();
 }
 
 function cartItemClickListener(event) {
-event.target.remove();
+  event.target.remove();
 }
 
 async function fetchAllProducts(productType) {
@@ -72,7 +64,14 @@ async function sendItemToCart(event) {
   document.querySelector('.cart__items').appendChild(cartElement);
 }
 
-window.onload = function onload() {
-  fetchAllProducts('computador');
+function createCartItemElement({ sku, name, salePrice }) {
+  const li = document.createElement('li');
+  li.className = 'cart__item';
+  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
+  li.addEventListener('click', cartItemClickListener);
+  return li;
 }
 
+window.onload = function onload() {
+  fetchAllProducts('computador');
+};
