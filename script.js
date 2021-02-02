543e1ca8e2bad13621a0e88b00ef08e5f115c83e
Henrique-Moura <henrique_hgm1996@hotmail.com>
2021-02-02 16:27:42 -0300

    Requisito Crie a listagem de produtos

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-window.onload = function onload() { };
-
 function createProductImageElement(imageSource) {
   const img = document.createElement('img');
   img.className = 'item__image';
@@ -26,18 +24,37 @@ function createProductItemElement({ sku, name, image }) {
   return section;
 }
 
-function getSkuFromProductItem(item) {
-  return item.querySelector('span.item__sku').innerText;
+// function getSkuFromProductItem(item) {
+//   return item.querySelector('span.item__sku').innerText;
+// }
+
+// function cartItemClickListener(event) {
+//   // coloque seu código aqui
+// }
+
+// function createCartItemElement({ sku, name, salePrice }) {
+//   const li = document.createElement('li');
+//   li.className = 'cart__item';
+//   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
+//   li.addEventListener('click', cartItemClickListener);
+//   return li;
+// }
+
+function fetchMercadoLivre(term) {
+  const itemElement = document.querySelector('.items');
+  const endpoint = `https://api.mercadolibre.com/sites/MLB/search?q=${term}`;
+  fetch(endpoint)
+    .then((response) => {
+      response.json()
+        .then((data) => {
+          data.results.forEach((element) => {
+            const { id: sku, title: name, thumbnail: image } = element;
+            itemElement.appendChild(createProductItemElement({ sku, name, image }));
+          });
+        });
+    });
 }
 
-function cartItemClickListener(event) {
-  // coloque seu código aqui
-}
-
-function createCartItemElement({ sku, name, salePrice }) {
-  const li = document.createElement('li');
-  li.className = 'cart__item';
-  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-  li.addEventListener('click', cartItemClickListener);
-  return li;
-}
+window.onload = function onload() {
+  fetchMercadoLivre('computador');
+};
