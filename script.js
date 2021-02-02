0e1693e9c2e5b79c3d8f3469e9dd80a95f43e36f
santiago <felipesantiagolds@gmail.com>
2021-02-02 16:37:21 -0300

    Requirement 2. Add the product to the shopping cart.

--- script.js
+++ script.js
@@ -26,8 +26,6 @@ function getSkuFromProductItem(item) {
   return item.querySelector('span.item__sku').innerText;
 }
 
-function cartItemClickListener(event) { }
-
 function createCartItemElement({ sku, name, salePrice }) {
   const li = document.createElement('li');
   li.className = 'cart__item';
@@ -37,15 +35,60 @@ function createCartItemElement({ sku, name, salePrice }) {
 }
 
 const retriveMercadoLivreApi = async () => {
-  const items = document.querySelector('.items');
-  const promise = await fetch('https://api.mercadolibre.com/sites/MLB/search?q=computador');
-  const response = await promise.json();
-  response.results.forEach((result) => {
-    const { id: sku, title: name, thumbnail: image } = result;
-    items.appendChild(createProductItemElement({ sku, name, image }));
+  try {
+    const items = document.querySelector('.items');
+    const promise = await fetch('https://api.mercadolibre.com/sites/MLB/search?q=computador');
+    const response = await promise.json();
+    response.results.forEach((result) => {
+      const { id: sku, title: name, thumbnail: image } = result;
+      items.appendChild(createProductItemElement({ sku, name, image }));
+    });
+
+    captureTargetItem();
+  } catch (error) { };
+};
+
+const cartItemClickListener = () => {
+  const itemEmpty = document.querySelector('.empty-cart');
+  itemEmpty.addEventListener('click', () => {
+    document.querySelector('.cart-items');
   });
 };
 
+const captureTargetItem = () => {
+  let target = '';
+
+  const itemAdd = document.querySelectorAll('.item__add');
+  itemAdd.forEach((element) => {
+    element.addEventListener('click', (event) => {
+      target = event.target.parentNode.querySelector('.item__sku').innerText;
+      console.log(target);
+      retrieveItems(target);
+    });
+  });
+};
+
+const retrieveItems = async (target) => {
+  const cartItem = document.querySelector('.cart__items');
+  try {
+    const promise = await fetch(`https://api.mercadolibre.com/items/${target}`);
+    const response = await promise.json();
+    console.log(response);
+    const { id: sku, title: name, base_price: salePrice } = response;
+    cartItem.appendChild(createCartItemElement({ sku, name, salePrice }));
+  } catch (error) { }
+};
+
+
 window.onload = function onload() {
   retriveMercadoLivreApi();
+  cartItemClickListener();
 };
+
+// Adicione o produto ao carrinho de compras
+// Cada produto na página HTML possui um botão com o nome Adicionar ao carrinho!.
+// Ao clicar nesse botão você deve realizar uma requisição para o endpoint: "https://api.mercadolibre.com/items/$ItemID" onde $ItemID deve ser o valor id do item selecionado.
+// Quando colocado o id MLB1341706310 retorno desse endpoint será algo no formato: elemento do objeto .result
+// Preste atenção que o JSON deve conter apenas um item.
+// Você deve utilizar a função createCartItemElement() para criar os componentes HTML referentes a um item do carrinho.
+// Adicione o elemento retornado da função createCartItemElement(product) como filho do elemento <ol class="cart__items">.
