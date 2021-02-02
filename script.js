775fadfc2fe92c5b740ff84d3950b7e6beb1e7ef
gabriel mendonca <gabriel.dev.lop@gmail.com>
2021-02-02 17:24:38 -0300

    Adicione o produto ao carrinho de compras finished requirement

--- script.js
+++ script.js
@@ -1,3 +1,26 @@
+function cartItemClickListener(event) {
+  // coloque seu código
+}
+
+function createCartItemElement({ sku, name, salePrice }) {
+  const li = document.createElement('li');
+  li.className = 'cart__item';
+  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
+  li.addEventListener('click', cartItemClickListener);
+  return li;
+}
+
+const addItemInCart = async (event) => {
+  const endPoint = `https://api.mercadolibre.com/items/${event.target.value}`;
+  const result = fetch(endPoint);
+  await result.then(resp => resp.json().then((res) => {
+    const { id: sku, title: name, price: salePrice } = res;
+    const li = createCartItemElement({ sku, name, salePrice });
+    const ol = document.querySelector('.cart__items');
+    ol.appendChild(li);
+  }));
+}
+
 function createProductImageElement(imageSource) {
   const img = document.createElement('img');
   img.className = 'item__image';
@@ -28,11 +51,14 @@ const queryItensInBd = async (params) => {
   const endPoint = `https://api.mercadolibre.com/sites/MLB/search?q=${params}`;
   const result = fetch(endPoint);
   await result.then(resp => resp.json().then((res) => {
-    res.results.map((item) => {
+    res.results.map((item, index) => {
       const { id: sku, title: name, thumbnail: image } = item;
       const section = createProductItemElement({ sku, name, image });
-      const ol = document.querySelector('.cart__items');
-      ol.appendChild(section);
+      const fatherSection = document.querySelector('.items');
+      fatherSection.appendChild(section);
+      const buttonAdd = document.querySelectorAll('.item__add')[index];
+      buttonAdd.addEventListener('click', addItemInCart)
+      buttonAdd.value = item.id;
       return undefined;
     });
   }));
@@ -40,20 +66,9 @@ const queryItensInBd = async (params) => {
 
 window.onload = function onload() {
   queryItensInBd('computador');
+  
 };
 
 function getSkuFromProductItem(item) {
   return item.querySelector('span.item__sku').innerText;
-}
-
-function cartItemClickListener(event) {
-  // coloque seu código
-}
-
-function createCartItemElement({ sku, name, salePrice }) {
-  const li = document.createElement('li');
-  li.className = 'cart__item';
-  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-  li.addEventListener('click', cartItemClickListener);
-  return li;
 }
