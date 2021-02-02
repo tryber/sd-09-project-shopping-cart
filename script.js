52b003f44c280c1fea82a17ee958d4409548bedb
Emanoel Leão <emanuelleaomarinho@gmail.com>
2021-02-02 00:00:18 -0300

    Alinhando ao code climate

--- script.js
+++ script.js
@@ -31,15 +31,16 @@ function createProductItemElement({ sku, name, image }) {
   return section;
 }
 
+function cartItemClickListener(event) {
+  // coloque seu código aqui
+}
+
 function createCartItemElement({ sku, name, salePrice }) {
   const li = document.createElement('li');
   li.className = 'cart__item';
   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
   li.addEventListener('click', cartItemClickListener);
   return li;
-}
-function cartItemClickListener(event) {
-  // coloque seu código aqui
 }
 const addToCart = async (event) => {
   const ol = document.querySelector('.cart__items');
@@ -71,8 +72,6 @@ const putElementsOnScreen = () => {
 function getSkuFromProductItem(item) {
   return item.querySelector('span.item__sku').innerText;
 }
-
-
 
 window.onload = function onload() {
   putElementsOnScreen();
