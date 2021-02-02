8357711ed27dea599ac35aa4570d966fc475fe11
Christopher <chris.tryber@gmail.com>
2021-02-02 17:00:11 -0300

    segundo requisito

--- script.js
+++ script.js
@@ -15,33 +15,54 @@ function createCustomElement(element, className, innerText) {
 function createProductItemElement({ sku, name, image }) {
   const section = document.createElement('section');
   section.className = 'item';
-
+  
   section.appendChild(createCustomElement('span', 'item__sku', sku));
   section.appendChild(createCustomElement('span', 'item__title', name));
   section.appendChild(createProductImageElement(image));
   section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
-
+  
   return section;
+}
+
+function addToCar() {
+  const buttonItemList = document.querySelectorAll('.item__add')
+  buttonItemList.forEach(specifcButton => {
+    specifcButton.addEventListener('click', () => {
+      let idSelected = specifcButton.parentNode.querySelector('.item__sku').innerText;
+      fetch(`https://api.mercadolibre.com/items/${idSelected}`)
+        .then(response => response.json())
+        .then(response => {
+          let objResult = {
+            sku: response.id,
+            name: response.title,
+            salePrice: response.price,
+          }
+          const ol = document.querySelector('.cart__items');
+          ol.appendChild(createCartItemElement(objResult));
+        })
+    })
+  })
 }
 
 function createListing(search) {
   const sectionItems = document.querySelector('.items');
   const promiseAPI = fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${search}`)
-  .then(response => response.json());
+    .then(response => response.json());
   const arrayResults = promiseAPI.then(response => response.results);
-  arrayResults.then(response => response.forEach((value) => {
-    resultOgj = {
-      sku: value.id,
-      name: search,
-      image: value.thumbnail,
-    };
-    sectionItems.appendChild(createProductItemElement(resultOgj));
-  }));
+  arrayResults
+    .then(response => {
+      response.forEach(value => {
+        resultOgj = {
+        sku: value.id,
+        name: value.title,
+        image: value.thumbnail,
+        };
+        sectionItems.appendChild(createProductItemElement(resultOgj));
+      })
+      //Chamar a função reuisito 2
+      addToCar()
+    });
 }
-
-window.onload = function onload() {
-  createListing('computador');
-};
 
 function getSkuFromProductItem(item) {
   return item.querySelector('span.item__sku').innerText;
@@ -58,3 +79,7 @@ function createCartItemElement({ sku, name, salePrice }) {
   li.addEventListener('click', cartItemClickListener);
   return li;
 }
+
+window.onload = function onload() {
+  createListing('computador');
+};
