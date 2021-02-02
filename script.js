baf387f85e0433a1600b5dc0d221bb41d1f9f715
Gabriel Castro <kastro.gabriel17@gmail.com>
2021-02-02 18:28:28 -0400

    requisito 2

--- script.js
+++ script.js
@@ -55,6 +55,29 @@ async function listProduct() {
   });
 }
 
+function addProductCart() {
+  const sectionButtons = document.querySelector('.items');
+  sectionButtons.addEventListener('click', async (event) => {
+    
+    const myId = getSkuFromProductItem(event.target.parentNode);
+    const apiProduct = `https://api.mercadolibre.com/items/${myId}`;
+
+    const response = await fetch(apiProduct);
+    const object = await response.json();
+
+      const objProduct = {
+        sku: myId,
+        name: object.title,
+        salePrice: object.price 
+      }
+   
+    const cartDad = document.querySelector('.cart__items');
+    const productToCart = createCartItemElement(objProduct);
+    cartDad.appendChild(productToCart);
+  });
+}
+
 window.onload = function onload() {
   listProduct();
+  addProductCart();
 };
