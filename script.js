01ebec4e5614d68e38c531e076d786d8875fd164
Danrlley Rodrigues <danrlley.c.a.rodrigues@gmail.com>
2021-02-02 16:20:07 -0300

    Corrigido CC 7

--- script.js
+++ script.js
@@ -1,15 +1,6 @@
-function createProductItemElement({ sku, name, image }) {
-  const section = document.createElement('section');
-  section.className = 'item';
-
-  section.appendChild(createCustomElement('span', 'item__sku', sku));
-  section.appendChild(createCustomElement('span', 'item__title', name));
-  section.appendChild(createProductImageElement(image));
-  section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
-
-  return section;
-}
-
+window.onload = function onload() {
+  retrieveMercadoLivreResults();
+};
 
 async function retrieveMercadoLivreResults(term) {
   const endpoint = `https://api.mercadolibre.com/sites/MLB/search?q=${term}`;
@@ -25,10 +16,6 @@ async function retrieveMercadoLivreResults(term) {
   });
 }
 
-window.onload = function onload() {
-  retrieveMercadoLivreResults();
-};
-
 function createProductImageElement(imageSource) {
   const img = document.createElement('img');
   img.className = 'item__image';
@@ -41,6 +28,18 @@ function createCustomElement(element, className, innerText) {
   e.className = className;
   e.innerText = innerText;
   return e;
+}
+
+function createProductItemElement({ sku, name, image }) {
+  const section = document.createElement('section');
+  section.className = 'item';
+
+  section.appendChild(createCustomElement('span', 'item__sku', sku));
+  section.appendChild(createCustomElement('span', 'item__title', name));
+  section.appendChild(createProductImageElement(image));
+  section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
+
+  return section;
 }
 
 function getSkuFromProductItem(item) {
