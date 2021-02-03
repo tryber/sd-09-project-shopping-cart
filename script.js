1e1037b170baad95de668eb399ac79a93f0cb34d
Fabio Venancio Almeida <fvalmeida.@gmail.com>
2021-02-03 15:46:48 -0300

    Acertos

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ async function retriveMercadoLivreResults(term) {
   const object = await response.json();
 
   const results = object.results;
-  const itemsElement = document.querySelector('.items')
+  const itemsElement = document.querySelector('.items');
 
   results.forEach((result) => {
     const { id: sku, title: name, thumbnail: image } = result;
@@ -33,25 +33,22 @@ function createCustomElement(element, className, innerText) {
 }
 
 function createProductItemElement({ sku, name, image }) {
-
   const section = document.createElement('section');
   section.className = 'item';
-
   section.appendChild(createCustomElement('span', 'item__sku', sku));
   section.appendChild(createCustomElement('span', 'item__title', name));
   section.appendChild(createProductImageElement(image));
   section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
-
   return section;
-};
+}
 
 function getSkuFromProductItem(item) {
   return item.querySelector('span.item__sku').innerText;
-};
+}
 
 function cartItemClickListener(event) {
   // coloque seu código aqui
-};
+}
 
 function createCartItemElement({ sku, name, salePrice }) {
   const li = document.createElement('li');
@@ -59,4 +56,4 @@ function createCartItemElement({ sku, name, salePrice }) {
   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
   li.addEventListener('click', cartItemClickListener);
   return li;
-};
+}
