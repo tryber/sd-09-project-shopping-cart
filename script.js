7d8a298f6f9a8adcb61986983f4531290847d16d
Pablo Fernandes <pabloprates@gmail.com>
2021-02-03 15:09:08 -0300

    Requirement six - Adding a button to clean cart

--- script.js
+++ script.js
@@ -77,6 +77,11 @@ window.onload = function onload() {
   getProductListFromAPIByQuerySearch('computador');
 };
 
+const btnEmptyCart = document.querySelector('.empty-cart');
+btnEmptyCart.addEventListener('click', () => {
+  const cartItems = document.querySelector('.cart__items');
+  cartItems.innerHTML = '';
+})
 const btnsAddItemToCart = document.querySelector('.items');
 btnsAddItemToCart.addEventListener('click', (event) => {
   if (event.target.className === 'item__add') {
