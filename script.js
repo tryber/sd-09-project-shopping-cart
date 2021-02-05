a93cd0c2efe4cb02831055d428c41a48259c8360
alailsonko <aalailson3@gmail.com>
2021-02-05 13:12:29 -0300

    add loading while fetching

--- script.js
+++ script.js
@@ -42,20 +42,39 @@ function createCartItemElement({ sku, name, salePrice }) {
   return li;
 }
 
+async function addItemsToCart(sku, name, loading, loadingElement) {
+  const cart = document.getElementsByClassName('cart__items')[0];
+  const fetchedProduct = await fetch(`https://api.mercadolibre.com/items/${sku}`);
+  let jsonifyProduct = {};
+  if (!jsonifyProduct.salePrice) {
+    console.log(loading)
+    jsonifyProduct = await fetchedProduct.json();
+  }
+  const { price: salePrice } = jsonifyProduct;
+  container.removeChild(loadingElement);
+  cart.appendChild(createCartItemElement({ sku, name, salePrice }));
+}
+
 async function LoadProducts() {
   const itemsUrl = 'https://api.mercadolibre.com/sites/MLB/search?q=computador';
-  const response = await (await fetch(itemsUrl)).json();
-  const products = response.results;
+  const response = await fetch(itemsUrl);
+  let jsonify = [];
+  const loading = createCustomElement('div', 'loading', 'loading...');
+  const container = document.getElementsByClassName('container')[0];
+  if (!jsonify.results) {
+    container.appendChild(loading);
+    jsonify = await response.json();
+  }
+  const loadingElement = document.getElementsByClassName('loading')[0];
+  container.removeChild(loadingElement);
+  const products = jsonify.results;
   const sectionItems = document.getElementsByClassName('items');
   products.forEach((product) => {
     const { id: sku, title: name, thumbnail: image } = product;
     const createdproduct = createProductItemElement({ sku, name, image });
     sectionItems[0].appendChild(createdproduct);
     createdproduct.childNodes[3].addEventListener('click', async () => {
-      const cart = document.getElementsByClassName('cart__items')[0];
-      const fetchedProduct = await (await fetch(`https://api.mercadolibre.com/items/${sku}`)).json();
-      const { price: salePrice } = fetchedProduct;
-      cart.appendChild(createCartItemElement({ sku, name, salePrice }));
+      await addItemsToCart(sku, name, loading, loadingElement);
     });
   });
 }
