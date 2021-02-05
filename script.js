878de05ad17789f65c87c14da1141db457a064b7
MarcosAlbuquerque <mpdsa20@gmail.com>
2021-02-05 19:31:33 -0300

    Produto adicionado ao carrinho

--- script.js
+++ script.js
@@ -54,6 +54,69 @@ async function loadAPI(find = 'computador') {
   return (Object.values(results).map(item => item).forEach(item => retrieveObjects(item)));
 }
 
+function addAttributesScripts() {
+  let button;
+  const promise = () => {
+    return new Promise((resolve, reject) => {
+      try {
+        setTimeout(() => {
+          button = document.querySelectorAll('section.item');
+          for(let i = 0; i < button.length; i += 1) {
+            document.querySelectorAll('section.item')[i].setAttribute('onclick','addItemsCart(this)');
+          };
+        }, 5000);
+        resolve('carregado');
+
+      } catch (error) {
+        throw new Error(error);
+      }
+    })
+  }
+
+  promise()
+}
+
+async function addItemsCart (tagHtml) {
+  const response = await fetch(`https://api.mercadolibre.com/items/${tagHtml.children[0].innerText}`);
+  const responseJSON = await response.json();
+  const results = responseJSON;
+  const filhoSection = document.querySelector('.cart__items');
+
+  filhoSection.appendChild(createCartItemElement(
+    {
+      sku: results.id,
+      name: results.title,
+      salePrice: results.price
+    }
+  ));
+}
+
+
 window.onload = function onload() {
   loadAPI();
+  addAttributesScripts()
+  
 };
+
+/**
+    Listagem de produtos
+      ✓ Listagem de produtos (428ms)
+    Adicione o produto ao carrinho de compras
+      1) Adicione o produto ao carrinho de compras
+    Remova o item do carrinho de compras ao clicar nele
+      2) Remova o item do carrinho de compras ao clicar nele
+    Carregue o carrinho de compras através do **LocalStorage** ao iniciar a página
+      3) Carregue o carrinho de compras através do **LocalStorage** ao iniciar a página
+    Some o valor total dos itens do carrinho de compras de forma assíncrona
+      4) Some o valor total dos itens do carrinho de compras de forma assíncrona
+    Botão para limpar carrinho de compras
+      5) Botão para limpar carrinho de compras
+    Adicionar um texto de "loading" durante uma requisição à API
+      6) Adicionar um texto de "loading" durante uma requisição à API
+
+
+    AssertionError: Timed out retrying: Expected to find element: ``, but never found it. Queried from element: <ol.cart__items>
+    AssertionError: Timed out retrying: Expected to find element: `.total-price`, but never found it.
+    AssertionError: Timed out retrying: Expected to find element: `.loading`, but never found it.
+
+ */ 
