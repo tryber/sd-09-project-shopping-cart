b2b0d904c2647e7dbdda6df29c640833975ca9f1
Maximilian Kaden <mkadenfilgo@gmail.com>
2021-02-06 17:31:48 -0300

    Adiciona o produto ao carrinho de compras

--- script.js
+++ script.js
@@ -30,21 +30,9 @@ function createProductItemElement({ sku, name, image }) {
   return section;
 }
 
-// function getSkuFromProductItem(item) {
-//   return item.querySelector('span.item__sku').innerText;
-// }
-
-// function cartItemClickListener(event) {
-//   // coloque seu código aqui
-// }
-
-// function createCartItemElement({ sku, name, salePrice }) {
-//   const li = document.createElement('li');
-//   li.className = 'cart__item';
-//   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-//   li.addEventListener('click', cartItemClickListener);
-//   return li;
-// }
+function getSkuFromProductItem(item) {
+  return item.querySelector('span.item__sku').innerText;
+}
 
 function addProductsOnList(productsList) {
   productsList.forEach((product) => {
@@ -55,6 +43,39 @@ function addProductsOnList(productsList) {
   });
 }
 
+function createCartItemElement({ sku, name, salePrice }) {
+  const li = document.createElement('li');
+  li.className = 'cart__item';
+  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
+  li.addEventListener('click', cartItemClickListener);
+  return li;
+}
+
+const fetchProductByID = (id) => {
+  const url = `https://api.mercadolibre.com/items/${id}`;
+
+  fetch(url)
+  .then(response => response.json())
+  .then((productData) => {
+    const { id: sku, title: name, price: salePrice } = productData;
+    const productParameter = { sku, name, salePrice };
+    const productElement = createCartItemElement(productParameter);
+    const cartList = document.querySelector('.cart__items');
+    cartList.appendChild(productElement);
+  });
+};
+
+// function cartItemClickListener(event) {
+//   // coloque seu código aqui
+// }
+
+
+function addProductOnCart(event) {
+  const itemClicked = event.target.parentNode;
+  const itemSku = getSkuFromProductItem(itemClicked);
+  fetchProductByID(itemSku);
+}
+
 const fetchProducts = (ProductToSearched) => {
   const url = `https://api.mercadolibre.com/sites/MLB/search?q=${ProductToSearched}`;
 
@@ -63,6 +84,8 @@ const fetchProducts = (ProductToSearched) => {
   .then((siteResponse) => {
     const productsList = siteResponse.results;
     addProductsOnList(productsList);
+    const buttons = document.querySelectorAll('.item__add');
+    buttons.forEach(button => button.addEventListener('click', addProductOnCart));
   });
 };
 
