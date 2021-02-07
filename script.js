1d4b00858e351d5055ba02f776aff0a463e8d0bd
Fabio Venancio Almeida <fvalmeida.@gmail.com>
2021-02-07 14:46:52 -0300

    ?

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+const itemsArrayLocalStorage = [];
+
 function createProductImageElement(imageSource) {
   const img = document.createElement('img');
   img.className = 'item__image';
@@ -56,29 +58,25 @@ async function retriveMercadoLivreResults(term) {
   descarregaLoading();
 }
 
-/* function saveCart() {
-  localStorage.setItem('cartItems', document.querySelectorAll('.cart__item'));
-} */
+function saveItemToLocalStorage(itemsArray) {
+  localStorage.setItem(0, itemsArray);
+}
 
-/* function createCartItemElement({ sku, name, salePrice }) {
-  const li = document.createElement('li');
-  li.className = 'cart__item';
-  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-  li.addEventListener('click', cartItemClickListener);
-  return li;
-} */
+function removeItemFromLocalStorage(itemID) {
+  itemIndex = itemsArrayLocalStorage.indexOf(itemID);
+  itemsArrayLocalStorage.splice(itemIndex, 1);
+  saveItemToLocalStorage(itemsArrayLocalStorage);
+}
 
-/* async function apiId(itemId) {
-  const endpoint = `https://api.mercadolibre.com/items/${itemId}`;
-
-  const response = await fetch(endpoint);
-  const object = await response.json();
-
-  const param = { sku: object.id, name: object.title, salePrice: object.price };
-  document.querySelector('.cart_items')
-  .appendChild(createCartItemElement(param));
-  saveCart();
-} */
+function cartItemClickListener(event) {
+  // coloque seu código aqui
+  if (event.target.className) {
+    const cartItems = document.querySelector('.cart__items');
+    cartItems.removeChild(event.target);
+    const itemID = extractItemID(event.target);
+    removeItemFromLocalStorage(itemID);
+  }
+}
 
 window.onload = function onload() {
   retriveMercadoLivreResults('computador');
@@ -87,8 +85,4 @@ window.onload = function onload() {
 
 function getSkuFromProductItem(item) {
   return item.querySelector('span.item__sku').innerText;
-}
-
-function cartItemClickListener(event) {
-  // coloque seu código aqui
 }
