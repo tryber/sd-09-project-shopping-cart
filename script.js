f71a3e6fc6c6a3e510d6d3c1bdf222ce161b85e2
Igor Machado <rf59imft@gmail.com>
2021-02-07 09:18:48 -0300

    desafio code climate 25

--- script.js
+++ script.js
@@ -14,38 +14,31 @@ function createLoading() {
   loadingText.innerText = 'loading...';
   document.body.appendChild(loadingText);
 }
-
 function stopLoading() {
   const loading = document.querySelector('.loading');
   loading.remove();
 }
-
 function createProductImageElement(imageSource) {
   const img = document.createElement('img');
   img.className = 'item__image';
   img.src = imageSource;
   return img;
 }
-
 function createCustomElement(element, className, innerText) {
   const e = document.createElement(element);
   e.className = className;
   e.innerText = innerText;
   return e;
 }
-
 function createProductItemElement({ sku, name, image }) {
   const section = document.createElement('section');
   section.className = 'item';
-
   section.appendChild(createCustomElement('span', 'item__sku', sku));
   section.appendChild(createCustomElement('span', 'item__title', name));
   section.appendChild(createProductImageElement(image));
   section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
-
   return section;
 }
-
 function getSkuFromProductItem(item) {
   return item.querySelector('span.item__sku').innerText;
 }
@@ -62,8 +55,7 @@ function createCartItemElement({ sku, name, salePrice }) {
   li.addEventListener('click', cartItemClickListener);
   return li;
 }
-
-function addCart() {
+function addToCart() {
   const items = document.querySelector('.items');
   items.addEventListener('click', async (event) => {
     const mySku = getSkuFromProductItem(event.target.parentNode);
@@ -82,14 +74,13 @@ function addCart() {
   });
 }
 
-async function getResults() {
+async function getMLResults() {
   const endpoint = 'https://api.mercadolibre.com/sites/MLB/search?q=computador';
   createLoading();
   const response = await fetch(endpoint);
   const object = await response.json();
   const results = object.results;
   const itemsElement = document.querySelector('.items');
-
   results.forEach((result) => {
     const obj = {
       sku: result.id,
@@ -101,7 +92,6 @@ async function getResults() {
   });
   stopLoading();
 }
-
 function emptyCart() {
   emptyCartButton = document.querySelector('.empty-cart');
   emptyCartButton.addEventListener('click', () => {
@@ -111,9 +101,10 @@ function emptyCart() {
 }
 
 window.onload = function onload() {
-  getResults();
-  addCart();
+  getMLResults();
+  addToCart();
   emptyCart();
   loadCart();
 };
+
 
