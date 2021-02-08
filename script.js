94eb616c9d6f1e546e5f64201af97bac31d6c8b4
Daniela Muller <danielamuller20@gmail.com>
2021-02-08 12:03:35 -0300

    ajusta requisito do preço total

--- script.js
+++ script.js
@@ -35,11 +35,11 @@ const updateCartPrice = async () => {
   const span = document.querySelector('.price');
   const items = document.querySelectorAll('.cart__item');
   const list = [...items];
-  const result = !list ? 0 : list.map((item) => {
+  const result = list.map((item) => {
     const text = item.innerText;
     return parseFloat(text.substring(text.indexOf('$') + 1));
   }).reduce((acc, cur) => acc + cur, 0);
-  span.innerText = !result ? 'Carrinho vazio' : `${Math.round(result * 100) / 100}`;
+  span.innerText = !result ? 'Carrinho vazio' : (Math.round(result * 1000) / 1000);
 };
 
 const saveLocalStorage = () => {
