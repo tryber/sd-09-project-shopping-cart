2564b2561327624a8378c04118c2b58f5fe290ca
Enio Nicael <enionicael@gmail.com>
2021-02-08 15:08:46 -0300

    Correcoes do code climate.

--- script.js
+++ script.js
@@ -30,6 +30,14 @@ function getSkuFromProductItem(item) {
   return item.querySelector('span.item__sku').innerText;
 }
 
+function createCartItemElement({ sku, name, salePrice }) {
+  const li = document.createElement('li');
+  li.className = 'cart__item';
+  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
+  li.addEventListener('click', cartItemClickListener);
+  return li;
+}
+
 const getPrice = (item) => {
   const index = item.indexOf('$');
   const price = item.slice(index + 1);
@@ -174,14 +182,6 @@ function cartItemClickListener({ target }) {
   cartProducts.splice(productIndex, 1);
   saveAtLocalStorage();
   displayProducts();
-}
-
-function createCartItemElement({ sku, name, salePrice }) {
-  const li = document.createElement('li');
-  li.className = 'cart__item';
-  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-  li.addEventListener('click', cartItemClickListener);
-  return li;
 }
 
 function handleClickClearButton() {
