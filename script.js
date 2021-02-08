c56ae07d10db8266c208dbf6b788dd576f92d9ce
Felipe Pimentel <lipe.pimentel89@gmail.com>
2021-02-08 20:17:38 -0300

    valor total async - codeclimate 2

--- script.js
+++ script.js
@@ -31,10 +31,9 @@ function getSkuFromProductItem(item) {
 let pricesArray = [];
 
 async function totalPrice(prices) {
-  const sumPrices = prices.reduce((total, price) => {
-    return Math.round(((Math.round(total * 100) / 100)
-    + (Math.round(price * 100) / 100)) * 100) / 100;
-  }, 0);
+  const sumPrices = prices.reduce((total, price) => 
+    Math.round(((Math.round(total * 100) / 100)
+    + (Math.round(price * 100) / 100)) * 100) / 100, 0);
   document.querySelector('.total-price').innerText = `Preço total: $${sumPrices}`;
 }
 
