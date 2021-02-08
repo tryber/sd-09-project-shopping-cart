e4513b83853f92d55fedadbee92a15663008f214
Daniela Muller <danielamuller20@gmail.com>
2021-02-08 10:51:45 -0300

    code climate

--- script.js
+++ script.js
@@ -35,12 +35,12 @@ const updateCartPrice = async () => {
   const span = document.querySelector('.price');
   const items = document.querySelectorAll('.cart__item');
   const list = [...items];
-  const result = list.map(item => {
+  const result = list.map((item) => {
     const text = item.innerText;
     return parseFloat(text.substring(text.indexOf('$') + 1));
   }).reduce((acc, cur) => acc + cur, 0);
   span.innerText = !result ? 'Carrinho vazio' : Math.round(result * 100) / 100;
-}
+};
 
 const saveLocalStorage = () => {
   const nodeList = document.querySelectorAll('.cart__item');
