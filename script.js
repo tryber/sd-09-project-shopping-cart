94051de88157dc23f8a3adf7113b47a038fff94f
Rodrigo Farias <deltadigo@gmail.com>
2021-02-08 20:48:46 -0300

    Requisito 3 - Remover o próprio item que foi clicado no carrinho

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function getSkuFromProductItem(item) {
 }
 
 function cartItemClickListener(event) {
-  // coloque seu código aqui
+  event.target.parentNode.removeChild(event.target);
 }
 
 function createCartItemElement({ sku, name, salePrice }) {
