baa7367d959835544a12f9704589727209db01d3
Matheus Schreiner <matheusrubisch@gmail.com>
2021-02-08 19:02:46 -0300

    teste

--- script.js
+++ script.js
@@ -5,6 +5,30 @@ function createProductImageElement(imageSource) {
   return img;
 }
 
+function cartItemClickListener(event) {
+  event.target.remove();
+}
+
+function createCartItemElement({ id: sku, title: name, price: salePrice }) {
+  const li = document.createElement('li');
+  li.className = 'cart__item';
+  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
+  li.addEventListener('click', cartItemClickListener);
+  return li;
+}
+
+function btnAddItem(event) {
+  const product = event.target.parentNode.firstChild.innerText;
+  fetch(`https://api.mercadolibre.com/items/${product}`)
+  .then((response) => {
+    response.json()
+    .then((object) => {
+      const li = createCartItemElement(object);
+      document.querySelector('.cart__items').appendChild(li);
+    });
+  });
+}
+
 function createCustomElement(element, className, innerText) {
   const e = document.createElement(element);
   e.className = className;
@@ -23,34 +47,6 @@ function createProductItemElement({ sku, name, image }) {
   section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
 
   return section;
-}
-
-function getSkuFromProductItem(item) {
-  return item.querySelector('span.item__sku').innerText;
-}
-
-function cartItemClickListener(event) {
-  event.target.remove();
-}
-
-function createCartItemElement({ id: sku, title: name, price: salePrice  }) {
-  const li = document.createElement('li');
-  li.className = 'cart__item';
-  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-  li.addEventListener('click', cartItemClickListener);
-  return li;
-}
-
-function btnAddItem(event) {
-  const product = event.target.parentNode.firstChild.innerText;
-  fetch(`https://api.mercadolibre.com/items/${product}`)
-  .then((response) => {
-    response.json()
-    .then((object) => {
-      const li = createCartItemElement(object);
-      document.querySelector('.cart__items').appendChild(li);
-    });
-  });
 }
 
 function createItems(object) {
