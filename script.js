3faf6055598b50aad714112e3ac0b6ae272dbe50
João Pedro Barros <joaopedrobarros5@gmail.com>
2021-02-09 12:21:13 -0300

    fix code climate 05

--- script.js
+++ script.js
@@ -5,16 +5,16 @@ function createProductImageElement(imageSource) {
   return img;
 }
 
+function cartItemClickListener(event) {
+  event.currentTarget.remove();
+}
+
 function createCartItemElement({ sku, name, salePrice }) {
   const li = document.createElement('li');
   li.className = 'cart__item';
   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
   li.addEventListener('click', cartItemClickListener);
   return li;
-}
-
-function cartItemClickListener(event) {
-  event.currentTarget.remove();
 }
 
 async function fetchCartItem(itemId) {
