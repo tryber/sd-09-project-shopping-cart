f3310ac5a3abbbb6b0a3f84d38da7a89cd42c61a
Pedro Amaral <amaralph1995@gmail.com>
2021-02-17 18:49:50 -0300

    retorna a estado anterior

--- script.js
+++ script.js
@@ -12,43 +12,6 @@ function createCustomElement(element, className, innerText) {
   return e;
 }
 
-function atualizaSoma(valor, sinal) {
-  const somador = document.querySelector('.somador');
-  if (sinal === '+') {
-    somador.innerHTML = Number(Math.floor(somador.innerHTML * 100) / 100) + valor;
-  } else {
-    somador.innerHTML = Number(Math.floor(somador.innerHTML * 100) / 100) - valor;
-  }
-  const partesSoma = somador.innerHTML.split('.');
-  const decimal = partesSoma[1];
-  if (decimal !== undefined) {
-    somador.innerHTML = `${partesSoma[0]}.${decimal.slice(0, 2)}`;
-  }
-  if (Number(somador.innerHTML) < 0) {
-    somador.innerHTML = '0';
-  }
-}
-
-function cartItemClickListener(event) {
-  // coloque seu código aqui
-  const item = event.target;
-
-  let preco = (Math.floor(item.innerText.split('PRICE: $')[1] * 100)) / 100;
-  preco = Math.floor(preco * 100) / 100;
-
-  atualizaSoma(preco, '-');
-
-  item.parentElement.removeChild(item);
-}
-
-function createCartItemElement({ sku, name, salePrice }) {
-  const li = document.createElement('li');
-  li.className = 'cart__item';
-  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-  li.addEventListener('click', cartItemClickListener);
-  return li;
-}
-
 function createProductItemElement({ sku, name, image }) {
   const section = document.createElement('section');
   section.className = 'item';
@@ -70,15 +33,18 @@ function createProductItemElement({ sku, name, image }) {
 
     const { price: salePrice } = object;
 
-    let preco = (Math.floor(salePrice * 100)) / 100;
-    preco = Math.floor(preco * 100) / 100;
-
-    atualizaSoma(preco, '+');
-
     cartItems.appendChild(createCartItemElement({ sku, name, salePrice }));
   });
 
   return section;
+}
+
+function createCartItemElement({ sku, name, salePrice }) {
+  const li = document.createElement('li');
+  li.className = 'cart__item';
+  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
+  li.addEventListener('click', cartItemClickListener);
+  return li;
 }
 
 async function createItems() {
@@ -99,13 +65,14 @@ async function createItems() {
 
 window.onload = function onload() {
   createItems();
-  const cart = document.querySelector('.cart');
-  const somador = document.createElement('div');
-  somador.className = 'somador';
-  somador.innerHTML = '0';
-  cart.appendChild(somador);
 };
 
 function getSkuFromProductItem(item) {
   return item.querySelector('span.item__sku').innerText;
+}
+
+function cartItemClickListener(event) {
+  // coloque seu código aqui
+  const item = event.target;
+  item.parentElement.removeChild(item);
 }
