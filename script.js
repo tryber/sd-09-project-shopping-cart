df5e64f6cbcd9f20aee23add8cb3d157ba2275b5
Bernardo Smith <bernardostech@gmail.com>
2021-02-19 09:22:21 -0500

    remover item unitario do carrinho

--- script.js
+++ script.js
@@ -25,16 +25,18 @@ function createProductItemElement({ sku, name, image }) {
 function getSkuFromProductItem(item) {
   return item.querySelector('span.item__sku').innerText;
 }
+*/
 
 function cartItemClickListener(event) {
+  event.target.remove();
 }
-*/
+
 
 function createCartItemElement({ id: sku, title: name, price: salePrice }) {
   const li = document.createElement('li');
   li.className = 'cart__item';
   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-  // li.addEventListener('click', cartItemClickListener);
+  li.addEventListener('click', cartItemClickListener);
   const local = {
     SKU: sku,
     NAME: name,
