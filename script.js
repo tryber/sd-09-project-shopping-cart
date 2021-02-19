c0f52042db3263e8a7a9974190e8321ddbe707c7
MarcosAlbuquerque <mpdsa20@gmail.com>
2021-02-19 15:03:30 -0300

    Correçao codeClimate

--- script.js
+++ script.js
@@ -56,7 +56,7 @@ async function priceItems(preco) {
   } else valorTotal += preco.price;
 
   const TAGSECTIONCART = document.getElementsByClassName('total-price')[0];
-  TAGSECTIONCART.innerHTML = valorTotal.toPrecision(2);
+  TAGSECTIONCART.innerHTML = (Number.isInteger(valorTotal)) ? Math.round(valorTotal) : valorTotal.toFixed(2);
 }
 
 function cartItemClickListener(event) {
