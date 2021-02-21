7062e523a0acb45698f4573c20d5fc13274ff4e4
MarcosAlbuquerque <mpdsa20@gmail.com>
2021-02-21 00:46:19 -0300

    Verificando test unitario do evaluator

--- script.js
+++ script.js
@@ -83,9 +83,10 @@ function cartItemClickListener(event) {
 function createCartItemElement({ sku, name, salePrice }) {
   const li = document.createElement('li');
   li.className = 'cart__item';
-  if (sku === 'MLB687124927') {
-    li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice + 1}`;
-  } else li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
+  // if (sku === 'MLB687124927') {
+  //   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice + 1}`;
+  // } else li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
+  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
   li.addEventListener('click', cartItemClickListener);
   return li;
 }
