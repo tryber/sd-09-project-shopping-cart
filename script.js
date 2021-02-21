24867c7e63c259f91b4b4430df5ca3d27d5124ec
MarcosAlbuquerque <mpdsa20@gmail.com>
2021-02-21 00:04:32 -0300

    Correçao codeClimate

--- script.js
+++ script.js
@@ -138,7 +138,7 @@ async function verifyLocalStorage() {
 
   if (itemList === undefined || itemList === null) return 0;
 
-  const produto = async id => sacola.push( fetch(urlAPI+id).then( res => res.json() ) );
+  const produto = async id => sacola.push(fetch(urlAPI + id).then(res => res.json()));
 
   itemList = JSON.parse(itemList);
   itemList.forEach(async (id) => {
