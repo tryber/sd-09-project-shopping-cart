23be3d3935ebc142c98ce937db6246a2d14280ff
Marina Alane <marina.alane@outlook.com>
2021-02-26 18:24:46 -0300

    ajusta btn

--- script.js
+++ script.js
@@ -59,7 +59,6 @@ async function mercadoLivreResults(term) {
   stopLoading();
 }
 
-
 function createCartListItem(itemList) {
   const cartItem = document.querySelector('.cart__items');
   cartItem.appendChild(itemList);
@@ -91,20 +90,20 @@ function addList() {
 
 // Limpar o carrinho de compras ao clicar no botão
 
-function setLocalStorage() {
-  const cartItem = document.querySelector('.cart__items');
-  localStorage.setItem('cartItem', cartItem.innerText);
-}
+// function setLocalStorage() {
+//   const cartItem = document.querySelector('.cart__items');
+//   localStorage.setItem('cartItem', cartItem.innerText);
+// }
 
-function getLocalStorage() {
-  document.querySelector('.cart__items').innerText = localStorage.getItem('cartItem');
-}
+// function getLocalStorage() {
+//   document.querySelector('.cart__items').innerText = localStorage.getItem('cartItem');
+// }
 
 function btnEmptyCart() {
   const btn = document.querySelector('.empty-cart');
   btn.addEventListener('click', function () {
-    const cartLists = document.querySelector('.cart_items');
-    cartLists.innerText = null;
+    const cartLists = document.querySelector('.cart__items');
+    cartLists.innerText = '';
   });
 }
 
