af33ee3faae4c2b48938ca2e15140026dd3251b0
LeonardoJosedaSilveira <rpgleo.js@gmail.com>
2021-03-24 01:18:13 -0300

    attempt to strike

--- script.js
+++ script.js
@@ -4,6 +4,14 @@
 
 // const fetch = require('node-fetch');
 
+// WORK STORAGE
+
+// function saveData() {
+//   localStorage.setItem('item_cart_id', 0);
+//   localStorage.setItem('total_price', 0);
+// }
+
+// INITIAL FUNCTIONS
 function createProductImageElement(imageSource) {
   const img = document.createElement('img');
   img.className = 'item__image';
@@ -44,18 +52,32 @@ function createProductItemElement({ sku, name, image, elementId }) {
 // }
 
 function cartItemClickListener(event) {
-  event.target.remove();
+  // cole seu codigo aqui
+  // drop value in total price
+  const texto = event.target.innerText;
+  const slaceIndex = texto.substring(texto.indexOf('$') + 1);
+  const totalPrice = parseInt(localStorage.getItem('total_price'), 10);
+  // console.log(totalPrice);
+  localStorage.setItem('total_price', totalPrice - slaceIndex);
+  // console.log(localStorage.total_price);
+
+  // drop item in cart shop
+  // event.target.remove();
+
+  // drop list cart
+  event.target.clearCart();
 }
 
-function createCartItemElement({ sku, name, salePrice }) {
+function createCartItemElement({ sku, name, salePrice, elementId }) {
   const li = document.createElement('li');
   li.className = 'cart__item';
   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
   li.addEventListener('click', cartItemClickListener);
+  li.id = `item_cart_${elementId}`;
   return li;
 }
 
-// adcionando item ao carrinho
+// ADD ITEM TO CART
 
 async function fechApiMlItem(sku) {
   const endpoint = `https://api.mercadolibre.com/items/${sku}`;
@@ -70,10 +92,16 @@ async function selectToCart(event) {
   const product = await fechApiMlItem(productID);
   const { id: sku, title: name, price: salePrice } = product;
   const sectionSelect = document.getElementsByClassName('cart__items');
-  sectionSelect[0].appendChild(createCartItemElement({ sku, name, salePrice }));
+  const elementId = parseInt(localStorage.item_cart_id, 10) + 1;
+  localStorage.setItem('item_cart_id', elementId);
+  const createElement = createCartItemElement({ sku, name, salePrice, elementId });
+  sectionSelect[0].appendChild(createElement);
+  // somando preço total
+  const totalPrice = parseInt(localStorage.total_price, 10) + salePrice;
+  localStorage.setItem('total_price', totalPrice);
 }
 
-// search
+// SEARCH
 
 async function fechApiMlSearch() {
   const endpoint = 'https://api.mercadolibre.com/sites/MLB/search?q=computador';
@@ -93,7 +121,60 @@ async function createProdutctsList() {
   });
 }
 
+// SHOW CART LIS PRICE
+
+// ----- pausa for other thins------
+
+async function listTotalPrice() {
+  const totalPriceSpan = document.getElementById('total-price-span');
+  // console.log(totalPriceSpan);
+  const totalPrice = localStorage.total_price;
+  totalPriceSpan.innerText = `Preço total: $${totalPrice}`;
+  // setTimeout(() => {
+  //   console.log(total_Price)
+  // }, 3000);
+}
+
+function cartListUpdate() {
+  window.addEventListener('click', listTotalPrice);
+}
+
+// async function testedocaraio(){
+//   console.log('asdfasd')
+// }
+
+// window.addEventListener('storage', () => {
+//   // When local storage changes, dump the list to
+//   // the console.
+//   testedocaraio();
+//   console.log('sampleList');
+// });
+
+// window.onstorage = () => {
+//   // When local storage changes, dump the list to
+//   // the console.
+//   console.log('sampleList');
+// };
+
+// window.localStorage = cartListUpdate
+
+// CLEAR LIST
+function clearCart() {
+  const clearButton = document.querySelector('.empty-cart');
+  clearButton.addEventListener('click', () => {
+    console.log('limpou ');
+    const li = document.querySelectorAll('.cart__item');
+    li.forEach((itemCart) => {
+      itemCart.remove();
+    });
+    localStorage.setItem('total_price', 0);
+  });
+}
+
 window.onload = function onload() {
   fechApiMlSearch();
   createProdutctsList();
+  // saveData();
+  cartListUpdate();
+  clearCart();
 };
